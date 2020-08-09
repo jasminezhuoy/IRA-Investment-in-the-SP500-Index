9c2f315ecf1f86cc650f3c926b09d2b29005c43e
Yue Zhuo <jasminezhuo1997@gmail.com>
2020-08-09 14:32:15 -0500

    Update CPI data

--- R/clean-CPI.R
+++ R/clean-CPI.R
@@ -3,7 +3,7 @@ CPI <- read_xlsx("datasets/inflation-CPI.xlsx", skip = 11)
 CPI_org <- data.frame()
 for (i in CPI$Year){
   CPI_sub <- CPI[CPI$Year==i,]
-  CPI_org_sub <- data.frame(Year = CPI$Year,
+  CPI_org_sub <- data.frame(Year = i,
                             Month = names(CPI_sub)[2:13],
                             CPI = t(CPI_sub)[2:13])
   CPI_org <- rbind(CPI_org, CPI_org_sub)
